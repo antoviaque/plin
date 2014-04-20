835571511dad42c4089ed63b2e834757db49137a
Xavier Antoviaque <xavier@antoviaque.org>
2014-04-20 18:35:20 -0400

    Add language to model and description page

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -3,6 +3,13 @@
 
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
+from django_languages.fields import LanguageField
+from south.modelsinspector import add_introspection_rules
+
+
+# South ###########################################################################################
+
+add_introspection_rules([], ["^django_languages\.fields\.LanguageField"])
 
 
 # Classes #########################################################################################
@@ -49,15 +56,15 @@ class Book(TimeStampedModel):
     illustrator = models.ForeignKey(Author, null=True, blank=True, related_name='book_illustrator_set')
     editor = models.ForeignKey(Editor)
     collection = models.ForeignKey(Collection, null=True, blank=True)
+    language = LanguageField()
     pub_date = models.DateTimeField('date published')
     reader_level = models.ForeignKey(ReaderLevel)
     nb_pages = models.IntegerField()
-    synopsis = models.TextField()
     cover = models.ImageField(upload_to='covers')
     keywords = models.ManyToManyField(Keyword)
+    synopsis = models.TextField()
 
     # TODO: use plugins for these fields
-    #language = models.CharField(max_length=2)
     #isbn = models.CharField(max_length=20)
 
     #votes = models.IntegerField(default=0)
