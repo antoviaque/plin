7e89f72dfc29dbfcaefb8684daf83550fd13398d
Xavier Antoviaque <xavier@antoviaque.org>
2014-06-08 20:59:33 -0400

    Switch to null

--- bookshelf/search_indexes.py
+++ bookshelf/search_indexes.py
@@ -11,13 +11,13 @@ from .models import Book
 class BookIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    subtitle = indexes.CharField(model_attr='subtitle', blank=True)
+    subtitle = indexes.CharField(model_attr='subtitle', null=True)
     isbn = indexes.CharField(model_attr='isbn')
     author = indexes.CharField(model_attr='author')
-    illustrator = indexes.CharField(model_attr='illustrator', blank=True)
+    illustrator = indexes.CharField(model_attr='illustrator', null=True)
     editor = indexes.CharField(model_attr='editor')
-    collection = indexes.CharField(model_attr='collection', blank=True)
-    keywords = indexes.CharField(model_attr='keywords', blank=True)
+    collection = indexes.CharField(model_attr='collection', null=True)
+    keywords = indexes.CharField(model_attr='keywords', null=True)
     synopsis = indexes.CharField(model_attr='synopsis')
 
     def get_model(self):
