ea5499d36ef84e879737fd8c6d6148dd8305c356
Xavier Antoviaque <xavier@antoviaque.org>
2014-06-08 20:56:44 -0400

    Allow some blank fields in Booki model from search

--- bookshelf/search_indexes.py
+++ bookshelf/search_indexes.py
@@ -11,13 +11,13 @@ from .models import Book
 class BookIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
-    subtitle = indexes.CharField(model_attr='subtitle')
+    subtitle = indexes.CharField(model_attr='subtitle', blank=True)
     isbn = indexes.CharField(model_attr='isbn')
     author = indexes.CharField(model_attr='author')
-    illustrator = indexes.CharField(model_attr='illustrator')
+    illustrator = indexes.CharField(model_attr='illustrator', blank=True)
     editor = indexes.CharField(model_attr='editor')
-    collection = indexes.CharField(model_attr='collection')
-    keywords = indexes.CharField(model_attr='keywords')
+    collection = indexes.CharField(model_attr='collection', blank=True)
+    keywords = indexes.CharField(model_attr='keywords', blank=True)
     synopsis = indexes.CharField(model_attr='synopsis')
 
     def get_model(self):
